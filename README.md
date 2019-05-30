b30adc806171187b36678e146e3c17116629d421
Yu Yang <yangyu823@gmail.com>
2019-05-30 16:51:19 +1000

    database design update

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 #   PriceTrackingHouse
 
-- [ ]   _Project No.1    ChemistWarehouse_
-    - [ ]   Stage Zero  (Preparation & Design)
+- [ ]   **Project No.1    ChemistWarehouse**
+    - [x]   Stage Zero  (Preparation & Design)
 
     - [ ]   Stage One   (Basic functionality)
     
-        - [ ] Database Design & Setup
+        - [x] Database Design & Setup
         - [ ] Check Individual Product price
         - [ ] Store Product Info into DB with timestamp
         - [ ] Separate API to check Price base on request link
@@ -22,10 +22,28 @@
     - [ ]   Stage Three (Back-end Polish and Automation)
     
         - [ ] Price Check mechanism automation(Daily Price Check)
-        - [ ] 
+        - [ ] To be continue
         
     - [ ]   Stage Four  (Front-end Polish and D3js)
 
 
-- [ ]   _Project No.2    Formula Price Add-on_
+- [ ]   __Project No.2    Formula Price Add-on__
     - [ ]   To be continue
+
+
+
+***
+
+### DataBase Structure:
+
+####price_db.product_cat
+| id   | vendor  | name  | last_update  |
+| -------- | -------- | -------- | -------- | 
+| cell 1  ­ ­ ­ ­  | cell 2  ­ ­ ­ ­ ­  | cell 3  ­ ­ ­ ­  | cell 4  ­ ­ ­ ­ |  
+####price_db.price_history
+| id   | vendor  | name  | price  | time  | image  | 
+| -------- | -------- | -------- | -------- | -------- | -------- | 
+| cell 1  ­ ­ ­ ­  | cell 2  ­ ­ ­ ­ ­  | cell 3  ­ ­ ­ ­  | cell 4  ­ ­ ­ ­  | cell 5  ­ ­ ­ ­  | cell 6  ­ ­ ­ ­  | 
+
+`imgae store as BLOB(binary). This is for price record evidence`
+
---
+++ getPrice/get_data.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+
+import os
+import requests
+import datetime
+from lxml import html
+from time import sleep
+from selenium import webdriver
+from get_agent import get_agent
+
+
+# from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
+# 禁用安全请求警告
+# requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+def get_image(url):
+    name = str((url.split('/')[-1]).replace('-', '_'))
+    print('Launching Firefox..')
+    date = (datetime.datetime.now().date())
+    print(date)
+    browser = webdriver.Firefox()
+    browser.get(url)
+    # screenshot = browser.save_screenshot("%s(%s).png" % (name, date))
+    screenshot_png = browser.get_screenshot_as_png()
+    print('Closing Firefox..')
+    browser.close()
+    return screenshot_png
+
+
+def get_data(url):
+    response = requests.get(url, timeout=2, headers={'User-Agent': get_agent()}).text
+    selector = html.fromstring(response)
+    product_id = ((selector.xpath('//*[@class="product-id"]/text()')[0]).split(':')[-1]).strip()
+    product_name = (selector.xpath('//*[@class="product-name"]/h1/text()')[0]).strip()
+    product_price = (selector.xpath('//*[@class="Price"]/span/text()')[0]).split("$")[-1]
+    capture_time = datetime.datetime.now().date()
+
+    # -----This is for evidence image-----
+    # product_img = get_image(url)
+
+    if "chemistwarehouse" in url:
+        product_vendor = 'ChemistWarehouse'
+    else:
+        product_vendor = 'Unknow'
+
+    print(product_vendor)
+
+
+if __name__ == '__main__':
+    link = 'https://www.chemistwarehouse.com.au/buy/65960/blackmores-omega-triple-concentrated-fish-oil-150-capsules'
+    get_data(link)
