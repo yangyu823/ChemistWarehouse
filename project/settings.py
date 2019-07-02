844dd4c2c6e17a23464dada45aea96e419279252
Yu Yang <yangyu823@gmail.com>
2019-07-02 11:45:32 +1000

    update

--- project/settings.py
+++ project/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'corsheaders',
+    'corsheaders',  # Setup for CORS START
     'backend'
 ]
 
@@ -48,10 +48,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',  # Setup for CORS START
 ]
 
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_ALLOW_ALL = True  # Setup for CORS START
 
 ROOT_URLCONF = 'project.urls'
 
